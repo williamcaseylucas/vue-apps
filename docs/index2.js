dfec491dbf5c649f36a67c522225d2990331212d
Blair MacIntyre <blair@macmynatt.com>
2021-06-09 14:22:55 -0400

    build

---
+++ docs/index2.js
@@ -0,0 +1,6 @@
+import {c as createApp} from "./vendor.js";
+import {_ as _sfc_main} from "./App2.js";
+import "./logo.js";
+let app = createApp(_sfc_main);
+app.mount("#app");
+app.$el.style.border = "solid 0.1em";
