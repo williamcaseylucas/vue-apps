367b07197497f63726710a5f0dd4cc747915825e
blairmacintyre <blair@macmynatt.com>
2021-06-20 16:58:35 +0000

    Deploying to gh-pages from @ realitymediabook/vue-apps@818c0fb7ad436912128787dbc284d2861e53056a 🚀

--- index3.js
+++ index3.js
@@ -1,6 +1,6 @@
 import { W as WebApp$1 } from "./WebApp.js";
 import { g as ref, h as onMounted, $, p as pushScopeId, b as popScopeId, o as openBlock, d as createBlock, e as createVNode, F as Fragment, w as withScopeId, r as reactive, a as readonly } from "./vendor.js";
-var App_vue_vue_type_style_index_0_scoped_true_lang = "\n#top[data-v-3b8c8b64] {\n  font-family: Avenir, Helvetica, Arial, sans-serif;\n  -webkit-font-smoothing: antialiased;\n  -moz-osx-font-smoothing: grayscale;\n  text-align: center;\n  color: #50312c;\n  margin-top: 30px;\n  border: 5;\n  border-radius: 10px;\n  border-color: red;\n  width: 400px; height: 300px;\n  position: absolute;\n\n  display: block;\n  background-color: white;\n}\n*[data-v-3b8c8b64] {\n    box-sizing: content-box;\n}\nimg[data-v-3b8c8b64] {\n    max-width: none;\n        display: inline;\n}\n";
+var App_vue_vue_type_style_index_0_scoped_true_lang = "\n#top[data-v-e5a378b8] {\n  font-family: Avenir, Helvetica, Arial, sans-serif;\n  -webkit-font-smoothing: antialiased;\n  -moz-osx-font-smoothing: grayscale;\n  text-align: center;\n  color: #50312c;\n  margin-top: 30px;\n  border: 5;\n  border-radius: 10px;\n  border-color: red;\n  width: 400px; height: 300px;\n  position: absolute;\n\n  display: block;\n  background-color: white;\n}\n*[data-v-e5a378b8] {\n    box-sizing: content-box;\n}\nimg[data-v-e5a378b8] {\n    max-width: none;\n        display: inline;\n}\n";
 const _sfc_main = {
   setup() {
     const root = ref(null);
@@ -43,14 +43,14 @@ const _sfc_main = {
   }
 };
 const _withId = /* @__PURE__ */ withScopeId();
-pushScopeId("data-v-3b8c8b64");
+pushScopeId("data-v-e5a378b8");
 const _hoisted_1 = /* @__PURE__ */ createVNode("link", {
   rel: "stylesheet",
-  href: "/test-vue-app/public/reveal/reveal.css"
+  href: "/vue-apps/public/reveal/reveal.css"
 }, null, -1);
 const _hoisted_2 = /* @__PURE__ */ createVNode("link", {
   rel: "stylesheet",
-  href: "/test-vue-app/public/reveal/theme/white.css"
+  href: "/vue-apps/public/reveal/theme/white.css"
 }, null, -1);
 const _hoisted_3 = {
   ref: "root",
@@ -78,7 +78,7 @@ const _sfc_render = /* @__PURE__ */ _withId((_ctx, _cache, $props, $setup, $data
   ], 64);
 });
 _sfc_main.render = _sfc_render;
-_sfc_main.__scopeId = "data-v-3b8c8b64";
+_sfc_main.__scopeId = "data-v-e5a378b8";
 class Store {
   constructor(app2) {
     this._state = reactive({
