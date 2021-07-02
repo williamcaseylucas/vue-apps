6c9cdea390f09627e23fa9d90486a29c2142327c
Jay Bolter <jdbolter@gatech.edu>
2021-07-02 09:25:02 -0400

    Changes to my vue Apps

--- hubs.js
+++ hubs.js
@@ -1,5 +1,13 @@
 import hubsTest1 from './src/apps/HubsTest1/hubs.js'
 import hubsTest2 from './src/apps/HubsTest2/hubs.js'
+import 3D from './src/apps/3D/hubs.js'
+import AR-VR from './src/apps/AR-VR/hubs.js'
+import Center1 from './src/apps/Center1/hubs.js'
+import Center2 from './src/apps/Center2/hubs.js'
+import History from './src/apps/History/hubs.js'
+import Intro from './src/apps/Intro/hubs.js'
+
+
 
 // need to wait until we have some reasonable performance optimizations
 // such as shadow dom support for Ethereal
@@ -12,4 +20,4 @@ import hubsTest2 from './src/apps/HubsTest2/hubs.js'
 // 		this.matrix.decompose( this.position, this.quaternion, this.scale )
 // 	}
 // }
-export {hubsTest1, hubsTest2}
+export {hubsTest1, hubsTest2, 3D, AR-VR, Center1,Center2, History, Intro}
