40d662e66fe8198bb48f0394ecdf286022dd8233
Jay Bolter <jdbolter@gatech.edu>
2021-07-08 13:43:02 -0400

    generated changes

--- docs/room.js
+++ docs/room.js
@@ -1,4 +1,4 @@
 import './room-dev-url.js';
 import 'https://jay-core.ngrok.io/build/index.js';
 
-window.__testingVueApps = "https://resources.realitymedia.digital/vue-apps/dist/hubs.js";
+window.__testingVueApps = "https://jay-vue-apps.ngrok.io/vue-apps/dist/hubs.js";
