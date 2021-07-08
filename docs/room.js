09d6136c05c1e3244a8a580b5b1b933596d12c08
Jay Bolter <jdbolter@gatech.edu>
2021-07-08 13:00:34 -0400

    Accepted Blair's changes
    New configuration of the servers. Just changed them to accept my names.

--- docs/room.js
+++ docs/room.js
@@ -1,4 +1,4 @@
 import './room-dev-url.js';
-import 'https://blair-core.ngrok.io/build/index.js';
+import 'https://jay-core.ngrok.io/build/index.js';
 
 window.__testingVueApps = "https://resources.realitymedia.digital/vue-apps/dist/hubs.js";
--- room.js
+++ room.js
@@ -4,4 +4,4 @@ window.__testingVueApps = "https://resources.realitymedia.digital/vue-apps/dist/
 import "./room-dev-url";
 
 //import 'https://resources.realitymedia.digital/core-components/build/index.js';
-import 'https://blair-core.ngrok.io/build/index.js';
+import 'https://jay-core.ngrok.io/build/index.js';
