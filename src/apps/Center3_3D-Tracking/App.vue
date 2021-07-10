3651485b4b114c2fff5658b5dcbec0c49d24bedd
Jay Bolter <jdbolter@gatech.edu>
2021-07-10 13:15:03 -0400

    Changes to apps for the images.

--- src/apps/Center3_3D-Tracking/App.vue
+++ src/apps/Center3_3D-Tracking/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="top">
     <h2> 3-D Graphics and the Construction of Visual Reality</h2>
-	<img src="../../assets/uncanny.jpg" width="200">
+	<img src="../../assets/images/uncanny.jpg" width="200">
 	<div class="displaytext">3-D computer graphics help to construct the visual realities of AR and VR, that is photorealism. The uncanny valley.</div>
 	</div>
 </template>
--- src/apps/Center_Map/App.vue
+++ src/apps/Center_Map/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="top">
   <Title msg="The LaCiotat Effect" />
-	<img src="../../assets/train.jpg" width="300" >
+	<img src="../../assets/images/train.jpg" width="300" >
 	<div>Film became one of the most important reality media of 
 	the twentieth century, and in some ways, it is a forerunner of virtual reality.</div>
 	  <div class="displaytext">AR allows us to extend our physical reality; VR creates for us a different reality.</div>
