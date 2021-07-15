368aed1becaa875ddc4b09811352562ee9cceb61
Blair MacIntyre <blair@macmynatt.com>
2021-07-15 17:37:15 -0400

    Delete docs directory

--- docs/room.js
+++
@@ -1,4 +0,0 @@
-import './room-dev-url.js';
-import 'https://resources.realitymedia.digital/core-components/build/index.js';
-
-window.__testingVueApps = "https://resources.realitymedia.digital/vue-apps/dist/hubs.js";
