dd87cb0b0c09b9316a077fb31b7244125f212663
Jay Bolter <jdbolter@gatech.edu>
2021-07-25 15:55:36 -0400

    got rid of the linkout component

--- src/apps/Center1_Intro/App.vue
+++ src/apps/Center1_Intro/App.vue
@@ -6,13 +6,17 @@
 	<div class="squareoff"> Each reality medium mediates and remediates. It offers a new representation of the world that we implicitly compare 
 		to our experience of the world in itself, but also through other media.</div> 
   </div>
-  <Linkout />
+   <p>
+    <a href="https://realitymedia.digital" target="_blank">
+      Start at the reality media site. 
+    </a>
+    |
+  </p>
   </div>
 </template>
 
 <script setup>
 import Title from '../../components/CenterTitle.vue'
-import Linkout from '../../components/Linkout.vue'
 
 import "../../assets/top.css"
 import "../../assets/room.css"
