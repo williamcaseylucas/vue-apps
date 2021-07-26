b2fc385009094e88ac30d5c9bae62ff3c4e8d96b
Jay Bolter <jdbolter@gatech.edu>
2021-07-26 16:27:26 -0400

    Adjusting spacing for Alyx poster

--- src/apps/Room5/Alyx/App.vue
+++ src/apps/Room5/Alyx/App.vue
@@ -2,6 +2,7 @@
   <div id="room">
   <div class="spacer">
   <img src="../../../assets/images/Room5/Alyx-splash.png" width="300" >
+  <br><br>
   <Title msg="HalfLife: Alyx" />
 	<div class="squareoff center"> First person shooter games such as  <a href="https://www.half-life.com/en/alyx/" target="_blank">HalfLife: Alyx </a> have long used 3-D graphics to create an immersive experience for millions of players. And for decades, 
     players on computers and game consoles have yearned for true VR so that they could fall through the screen into the worlds on the other side.</div> 
