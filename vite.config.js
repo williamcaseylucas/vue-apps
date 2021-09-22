ffab1aa180e3000e1e140f33b672a8aa864a882b
Blair MacIntyre <blair@macmynatt.com>
2021-09-22 10:44:17 -0400

    css fixes, mostly

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,20 @@ export default defineConfig({
         index1: resolve(__dirname, 'src/apps/HubsTest1/index.html'),
         index2: resolve(__dirname, 'src/apps/HubsTest2/index.html'),
         index3: resolve(__dirname, 'src/apps/Center_Map/index.html'),
+        index4: resolve(__dirname, 'src/apps/Center1_Intro/index.html'),
+        index5: resolve(__dirname, 'src/apps/Center2_History/index.html'),
+        index6: resolve(__dirname, 'src/apps/Center3_3D-Tracking/index.html'),
+        index7: resolve(__dirname, 'src/apps/Center4_Presence/index.html'),
+        index8: resolve(__dirname, 'src/apps/Center5_Genres/index.html'),
+        index9: resolve(__dirname, 'src/apps/Center6_Future/index.html'),
+        index10: resolve(__dirname, 'src/apps/Center7_Privacy/index.html'),
+        index11: resolve(__dirname, 'src/apps/Monolith1_Intro/index.html'),
+        index12: resolve(__dirname, 'src/apps/Monolith2_History/index.html'),
+        index13: resolve(__dirname, 'src/apps/Monolith3_3D-Tracking//index.html'),
+        index14: resolve(__dirname, 'src/apps/Monolith4_Presence/index.html'),
+        index15: resolve(__dirname, 'src/apps/Monolith5_Genres/index.html'),
+        index16: resolve(__dirname, 'src/apps/Monolith6_Future/index.html'),
+        index17: resolve(__dirname, 'src/apps/Monolith7_Privacy/index.html'),
         hubs: resolve(__dirname, 'hubs.ts')
       },
     //   external: ['three'],
@@ -47,7 +61,7 @@ export default defineConfig({
             typescript: require('typescript'),
         }),
          
-          node_resolve(),
+        node_resolve(),
       ]
     }
   },
