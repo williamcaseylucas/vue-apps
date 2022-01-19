e2da4daecb536d243a3dbad061b081a9b5e73fe8
Jisu Park <jspark474@gmail.com>
2022-01-19 14:25:55 -0500

    Text change for Presence and Aura room

--- src/apps/Room6/Aura/App.vue
+++ src/apps/Room6/Aura/App.vue
@@ -10,6 +10,7 @@
 	<div class="squareoff">
     <p>In 1930s, Walter Benjamin introduced the concept of <em>aura</em> in The Work of Art in the Age of Mechanical Reproduction. 
   Aura is the <em>here and now</em> that work possesses because of its unique history of production and transmissinowon. </p>
+  <br>
   <p>AR applications are not perfect reproductive technologies, as some draw on the physical and cultural uniquesness, <em>the here and now</em> of particular places </p>
   </div> 
   </div>
