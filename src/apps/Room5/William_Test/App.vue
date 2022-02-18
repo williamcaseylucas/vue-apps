5a50d7bd2b15f41b99aebe4bafd638077242dbe1
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2022-02-18 11:14:16 -0500

    Added sample William Room

---
+++ src/apps/Room5/William_Test/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div> 
+  <div id="room" class="darkwall">
+  <div class="spacer">
+  <img src="../../../assets/images/Room6/Casa-batllo.jpg" width="400" >
+  <br><br>
+  <!-- <Title msg="HalfLife: Alyx" /> -->
+	<div class="a" id="room"> This is a sample file</div> 
+  <button style="margin-top: 10px">This is a button</button>
+  </div>
+  </div>
+  </div> 
+</template>
+
+<script setup>
+import Title from '../../../components/CenterTitle.vue'
+
+import "../../../assets/top.css"
+import "../../../assets/room.css"
+</script>
+
+<style scoped>
+
+</style>
