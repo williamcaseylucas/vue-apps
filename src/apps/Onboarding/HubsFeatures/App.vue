80ac31326ea3573c3097896dd628d590a64b7cb0
Jisu Park <jspark474@gmail.com>
2022-02-18 16:47:30 -0500

    Change dimension on one image

--- src/apps/Onboarding/HubsFeatures/App.vue
+++ src/apps/Onboarding/HubsFeatures/App.vue
@@ -7,7 +7,7 @@
 <br>
 	<div class="squareoff"> The figure below indicates how to mute your microphone, take photos, share your screen, create media objects, and so on: </div> 
     <br><br>
-    <div width="400">
+    <div style="width:400px">
       <img xr-layer src="../../../assets/images/Onboarding/hubs-user-interface.jpg">
     </div>
 
