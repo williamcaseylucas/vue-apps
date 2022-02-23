63db1be3b2458c846aea4380512e24e78b9542d6
Jisu Park <jspark474@gmail.com>
2022-02-23 10:36:44 -0500

    Add xr-layer to anchor tags

--- src/apps/Room6/Nonnie/App.vue
+++ src/apps/Room6/Nonnie/App.vue
@@ -9,7 +9,7 @@
   <Title msg="The future of news?" />
   <br>
   <br>
-	<div class="squareoff">Nonnie de la Peña's <span class="alink"><a href="https://embed.ted.com/talks/nonny_de_la_pena_the_future_of_news_virtual_reality" target="_blank">Ted Talk</a></span> called 'The future of news?''  introduces a new form of journalism where Virtual Reality technology is used to put audience inside the stories. In her work, she created VR stories about imprisonment in Guantanamo and hunger in Los Angeles to induce empathy in the audience.</div> 
+	<div class="squareoff">Nonnie de la Peña's <span xr-layer><a href="https://embed.ted.com/talks/nonny_de_la_pena_the_future_of_news_virtual_reality" class="alink" target="_blank">Ted Talk</a></span> called 'The future of news?''  introduces a new form of journalism where Virtual Reality technology is used to put audience inside the stories. In her work, she created VR stories about imprisonment in Guantanamo and hunger in Los Angeles to induce empathy in the audience.</div> 
   </div>
   </div>
   </div> 
