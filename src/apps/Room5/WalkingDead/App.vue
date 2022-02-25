88559c58356dc358f9019a0daa93595241fbbf77
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2022-02-25 01:03:25 -0500

    Testing color change and custom image

--- src/apps/Room5/WalkingDead/App.vue
+++ src/apps/Room5/WalkingDead/App.vue
@@ -2,7 +2,9 @@
   <div id="room" class="darkwall">
   <div class="spacer">
   <Title msg="Walking Dead: Our World" />
-	  <div class="squareoff">In this AR version of the transmedia franchise
+  <!-- Take out color red -->
+    <img src="../../assets/images/Room5/Walking Dead Image.png" alt="">
+	  <div class="squareoff" style="color: red;">In this AR version of the transmedia franchise
       GPS is used to determine your location in the world. Your location 
       and the zombies appear in an enhanced Google Maps map on the phone screen.
     </div> 
