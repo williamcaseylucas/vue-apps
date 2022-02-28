d7b10f6c2158615caf6f8dafc6b981f20228eff7
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2022-02-27 21:05:24 -0500

    Update App.vue

--- src/apps/Room5/WalkingDead/App.vue
+++ src/apps/Room5/WalkingDead/App.vue
@@ -6,7 +6,7 @@
     <a target="_blank" href="https://www.thewalkingdeadourworld.com/">
     <img style="width: 100%; height: auto;" src="../../../assets/images/Room5/Walking_Dead_Image.png" alt="Walking Dead Image">
     </a>
-	  <div class="squareoff">TEST! In this AR version of the transmedia franchise
+	  <div class="squareoff">In this AR version of the transmedia franchise
       GPS is used to determine your location in the world. Your location 
       and the zombies appear in an enhanced Google Maps map on the phone screen.
     </div> 
