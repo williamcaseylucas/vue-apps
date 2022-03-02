9b28ae585dd13cbd6530ebcbf6565a9755f373f5
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2022-03-02 00:05:06 -0500

    Bug fix attempt
    debugging

--- src/apps/Room5/Pokemon/App.vue
+++ src/apps/Room5/Pokemon/App.vue
@@ -2,7 +2,7 @@
   <div id="room" class="darkwall">
   <div class="spacer">
   <a target="_blank" href="https://pokemongolive.com/en/">
-  <img xr-layer width="400" src="../../../assets/images/Room5/Pokemon_go_image.jpg" alt="Pokemon Go Image">
+  <img xr-layer width="400" src="../../../assets/images/Room5/Walking_Dead_Image.png" alt="Pokemon Go Image">
   </a>
   <Title msg="Pokemon Go" />
 	  <div class="squareoff">Pokemon Go (2016) is perhaps still the best-known AR game. 
