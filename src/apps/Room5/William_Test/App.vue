9213d4f50cb877743dc1a1d375bd41c86996afce
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2022-03-17 16:13:15 -0400

    update

--- src/apps/Room5/William_Test/App.vue
+++ src/apps/Room5/William_Test/App.vue
@@ -5,7 +5,7 @@
   <img src="../../../assets/images/Room6/Casa-batllo.jpg" width="400" >
   <br><br>
   <!-- <Title msg="HalfLife: Alyx" /> -->
-	<div class="a" id="room"> This is a sample file</div> 
+	<div class="a" id="room"> This is a sample file </div> 
   <button style="margin-top: 10px">This is a button</button>
   </div>
   </div>
