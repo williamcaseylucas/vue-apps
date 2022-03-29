9036ae6dd3450d362a6ad0cf2736c2465317a402
Blair MacIntyre <blair@macmynatt.com>
2022-03-29 15:08:46 -0400

    fixed some xr-layers for testing

--- src/apps/Onboarding/HubsPlatform2/App.vue
+++ src/apps/Onboarding/HubsPlatform2/App.vue
@@ -12,12 +12,12 @@
 <br>
 - On a VR headset, if you opened the URL on your desktop or smartphone, choose "Enter on Standalone VR" to create a code that makes it easy to open on your standalone headset. Open the browser in your VR headset, navigate to hubs.link and enter the code.
 <br><br>
-<div xr-layer>
+<div>
 <div class="keyPoint">To navigate in Hubs:</div>  
 <br>
 - On desktop use your WASD or arrow keys to move around. You can also press your right mouse button to teleport to a different location. Rotate your view using the Q and E keys, or hold down your left mouse button and drag.
 <br>
-- For VR and mobile controls, see the <a href="https://hubs.mozilla.com/docs/hubs-controls.html" target="blank" class="alink">list of Hubs controls.</a>
+- For VR and mobile controls, see the <a href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank" class="alink">list of Hubs controls.</a>
  </div>
   </div>
   </div>
