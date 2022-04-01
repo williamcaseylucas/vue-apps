c0cbe835063adcb91f9e4fac9a1eff7951c286cf
Jisu Park <jspark474@gmail.com>
2022-04-01 18:48:37 -0400

    fix

--- src/apps/Onboarding/HubsPlatform/App.vue
+++ src/apps/Onboarding/HubsPlatform/App.vue
@@ -4,7 +4,7 @@
   <div class="spacer">
   <div style="margin: 0 auto; display: block">
     <img xr-layer src="../../../assets/images/Onboarding/MozillaHubs.jpg" height="212">
-  <div>
+  </div>
   <br><br>
   <Title msg="The Hubs Platform" />
   <br />
