93ed6721a0859d593c1bd1826b2b23cb6458dcc8
Jisu Park <jspark474@gmail.com>
2022-04-03 22:03:47 -0400

    Fix css on one app in presence gallery

--- src/apps/Room6/Laciotat/App.vue
+++ src/apps/Room6/Laciotat/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div> 
   <div id="room" class="darkwall">
+  <div class="spacer-side">
+  <div class="squareoff">
     <div>Reality media have always been about presence. The legend of La Ciotat is a myth of presence, which you can explore in the gallery entitled "What are Reality Media?"</div>
+  </div>
+  </div>
   </div>
   </div> 
 </template>
