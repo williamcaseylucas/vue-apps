4680f47d0f624ac9b0de8dddcb406c94806ad7b0
Jisu Park <jspark474@gmail.com>
2022-04-19 09:54:29 -0400

    Revert "Revert Onboarding pictures"
    This reverts commit b065c053174716be2786444cc654570e76bb431b.

--- src/apps/Onboarding/MitPress/App.vue
+++ src/apps/Onboarding/MitPress/App.vue
@@ -4,16 +4,14 @@
   <div class="spacer">
   <Title msg="Reality Media" />
   <br />
-  
-  <div style="float:left; margin-right:20px">
-  <img xr-layer src="../../../assets/images/Onboarding/realityMediaBook.jpg" height="383">
-  </div>
-
-  <div class="squareoff"><div style="margin-left:30px">Published by <a href="https://mitpress.mit.edu/books/reality-media" target="_blank" class="alink">MIT Press</a></div>
+<br />
+<div style="width: 50%; float: right">
+  <div class="squareoff">Published by <a href="https://mitpress.mit.edu/books/reality-media" target="_blank" class="alink">MIT Press</a>
   <div class="oblique">By Jay David Bolter, Maria Engberg and Blair MacIntyre</div> 
   <br>
+  <br>
   How augmented reality and virtual reality are taking their places in contemporary media culture alongside film and television.</div>
-
+</div>
   </div>
   </div>
   </div> 
