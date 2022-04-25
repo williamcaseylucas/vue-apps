129ff38a7fe48d835ddd5742b9c95b705cb28339
Jisu Park <jspark474@gmail.com>
2022-04-25 02:24:55 -0400

    test a

--- src/apps/Onboarding/HubsPlatform/App.vue
+++ src/apps/Onboarding/HubsPlatform/App.vue
@@ -4,7 +4,7 @@
   <div class="spacer">
   <Title msg="The Hubs Platform" />
   <br />
-	<div class="squareoff"><em>Realitymedia</em> is built on top of Mozilla's open-source platform. An extensive guide to using Mozilla Hubs is available <span xr-layer><a href="https://hubs.mozilla.com/docs/intro-hubs.html" target="_blank" class="alink">in the Hubs user documentation.</a></span><br><br>
+	<div class="squareoff"><em>Realitymedia</em> is built on top of Mozilla's open-source platform. An extensive guide to using Mozilla Hubs is available <a href="https://hubs.mozilla.com/docs/intro-hubs.html" target="_blank" class="alink"><span xr-layer class="test">in the Hubs user documentation.</span></a><br><br>
   Here are the highlights:
   <br><br>
 Before entering, you are in the room's lobby. From here, you can see and hear what's going on inside the room, but you can only interact with others using text chat. 
@@ -21,7 +21,16 @@ import Title from '../../../components/CenterTitle.vue'
 import "../../../assets/top.css"
 import "../../../assets/room.css"
 </script>
-
+.test{
+  color: #fdfd96;
+  text-decoration: underline;
+  text-align: left;
+}
+.test:hover{
+  color: white;
+  background:#78cfa8;
+  text-align: left;
+}
 <style scoped>
 
 </style>
--- src/apps/Onboarding/HubsPlatform2/App.vue
+++ src/apps/Onboarding/HubsPlatform2/App.vue
@@ -17,7 +17,7 @@
 <br>
 - On desktop use your WASD or arrow keys to move around. You can also press your right mouse button to teleport to a different location. Rotate your view using the Q and E keys, or hold down your left mouse button and drag.
 <br>
-- For VR and mobile controls, see the <a xr-layer href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank" class="alink"><span xr-layer>list of Hubs controls.</span></a>
+- For VR and mobile controls, see the <a xr-layer href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank"><span xr-layer class="test">list of Hubs controls.</span></a>
  </div>
   </div>
   </div>
