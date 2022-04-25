db6aca9e0235b1349246be7263d1333bf39b724f
Jisu Park <jspark474@gmail.com>
2022-04-25 02:44:31 -0400

    test 2

--- src/apps/Onboarding/HubsPlatform2/App.vue
+++ src/apps/Onboarding/HubsPlatform2/App.vue
@@ -2,7 +2,7 @@
   <div> 
   <div id="room" class="darkwall">
   <div class="spacer">
-    <div class="squareoff">
+    <div xr-layer class="squareoff">
       <br>
       <br>
       <br>
@@ -17,7 +17,7 @@
 <br>
 - On desktop use your WASD or arrow keys to move around. You can also press your right mouse button to teleport to a different location. Rotate your view using the Q and E keys, or hold down your left mouse button and drag.
 <br>
-- For VR and mobile controls, see the <a href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank"><span xr-layer class="test">list of Hubs controls.</span></a>
+- For VR and mobile controls, see the <a href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank" class="alink">list of Hubs controls.</a>
  </div>
   </div>
   </div>
@@ -31,14 +31,7 @@ import Title from '../../../components/CenterTitle.vue'
 import "../../../assets/top.css"
 import "../../../assets/room.css"
 </script>
-.test{
-  color: #fdfd96;
-  text-decoration: underline;
-}
-.test:hover{
-  color: white;
-  background:#78cfa8;
-}
+
 <style scoped>
 
 </style>
