b99f94ea0bd74eab346f66e9320b30d2a7fd02c7
Jisu Park <jspark474@gmail.com>
2022-04-25 00:55:42 -0400

    Add quotes to Presence gallery and fix anchor style

---
+++ src/apps/Room6/Quote4/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div> 
+  <div id="room" class="darkwall">
+  <div class="spacer-side">
+	<div class="squareoff">
+   <div class="lgquote">VR experiences generally lack aura, but AR experiences for cultural heritage can borrow or even enhance the aura of place.</div>
+  </div> 
+  </div>
+  </div>
+  </div> 
+</template>
+
+<script setup>
+import Title from '../../../components/CenterTitle.vue'
+
+import "../../../assets/top.css"
+import "../../../assets/room.css"
+</script>
+
+<style scoped>
+
+</style>
