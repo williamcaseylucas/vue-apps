7f155a60c474cf57c328e2ee78557f2e27906116
Jisu Park <jspark474@gmail.com>
2022-04-26 01:09:47 -0400

    test contd

--- src/apps/Onboarding/HubsPlatform2/App.vue
+++ src/apps/Onboarding/HubsPlatform2/App.vue
@@ -17,10 +17,6 @@
 <br>
 <!--- On desktop use your WASD or arrow keys to move around. You can also press your right mouse button to teleport to a different location. Rotate your view using the Q and E keys, or hold down your left mouse button and drag. -->
 <br>
-- For VR and mobile controls, see the <span class="test3"><a xr-layer href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank">list of Hubs controls.</a></span>
-<br/>
-- For VR and mobile controls, see the <span class="test1"><a xr-layer href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank" class="alink">list of Hubs controls.</a></span>
-<br/>
 - For VR and mobile controls, see the <a xr-layer href="https://hubs.mozilla.com/docs/hubs-controls.html" target="_blank" class="alink">list of Hubs controls.</a>
 <br/>
  </div>
